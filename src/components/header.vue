92acfb407511a81e6a2831c609a0d1e057ad5710
suraj kumar <surajshah766@gmail.com>
2020-09-10 23:09:12 +0530

    Second Commit

---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <nav class="navbar navabr-light bg-light">
+            <a href="#" class="navbar-brand">
+                <img src="../assets/logo.png" alt="" height="20px">
+                Test
+            </a>
+        </nav>
+    </div>
+</template>
+    <script>
+    export default {
+        name:'header',
+    }
+    </script>
